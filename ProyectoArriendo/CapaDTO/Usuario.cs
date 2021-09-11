161769f7650707a7f05e97907bd9f2148c2818d4
VelizManuel <58229481+VelizManuel@users.noreply.github.com>
2021-09-11 19:08:55 -0300

    Add files via upload

---
+++ ProyectoArriendo/CapaDTO/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDTO
+{
+    public class Usuario
+    {
+        private int id_usuario;
+        private string rut_usuario;
+        private string tipo_usuario;
+        private string clave;
+
+        public int Id_usuario { get => id_usuario; set => id_usuario = value; }
+        public string Rut_usuario { get => rut_usuario; set => rut_usuario = value; }
+        public string Tipo_usuario { get => tipo_usuario; set => tipo_usuario = value; }
+        public string Clave { get => clave; set => clave = value; }
+    }
+}
